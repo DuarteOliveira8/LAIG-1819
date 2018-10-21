34ce7e0a359783d55d99561c23869003c1425362
Duarte Oliveira <Duda8Filipe@gmail.com>
2018-10-21 15:46:16 +0100

    cameras definition

--- XMLscene.js
+++ XMLscene.js
@@ -40,8 +40,6 @@ class XMLscene extends CGFscene {
      * Initializes the scene cameras.
      */
     initCameras() {
-        // this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30, 30, 30), vec3.fromValues(0, 0, 0));
-        // this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30, 10, 30), vec3.fromValues(0, 0, 0));
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(5, 30, 50), vec3.fromValues(5, 0, 0));
     }
     /**
