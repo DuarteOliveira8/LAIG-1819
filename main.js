4c924587117a6ac6b95b219ba64f197481bd0eab
Duarte Oliveira <Duda8Filipe@gmail.com>
2018-10-21 19:15:54 +0100

    Changed yas file name

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "yas.xml";
+	var filename=getUrlVars()['file'] || "ribeira.xml";
 
 	// create and load graph, and associate it to scene.
 	// Check console for loading errors
