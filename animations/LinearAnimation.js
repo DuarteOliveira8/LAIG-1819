07adf6d45866c1f5722435c4231575785548ebff
pedro <pedrokraktuga@gmail.com>
2018-11-19 23:19:42 +0000

    Progress on LinearAnimation

--- animations/LinearAnimation.js
+++ animations/LinearAnimation.js
@@ -17,13 +17,23 @@ class LinearAnimation extends Animation {
     this.controlPoints = controlPoints;
     this.currentPoint = 0;
 
+		let firstPoint = vec3.create();
+		vec3.set(firstPoint, controlPoints[0].x, controlPoints[0].y, controlPoints[0].z);
+		vec3.normalize(firstPoint, firstPoint);
+
+		let objectDir = vec3.create();
+		vec3.set(objectDir, 0, 0, 1); //z axis
+
+		let rotateAngle = Math.atan2(firstPoint[2], firstPoint[0]) - Math.atan2(objectDir[2], objectDir[0]);
+
+		mat4.rotateY(this.transformation, this.transformation, rotateAngle);
+		mat4.translate(this.transformation, this.transformation, firstPoint);
+
 		this.initBuffers();
   };
 
 	apply() {
-    // let previousPoint = (this.currentPoint-1) == -1 ? (this.controlPoints.length-1) : (this.currentPoint-1);
 
-    // this.scene.translate(this.controlPoints[this.currentPoint].x, this.controlPoints[this.currentPoint].y, this.controlPoints[this.currentPoint].z)
 	};
 
 	copy() {
