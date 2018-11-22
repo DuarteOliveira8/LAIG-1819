3450cae2159411e3f03cd3f0996dc9b559f232d6
pedro <pedrokraktuga@gmail.com>
2018-11-22 16:24:11 +0000

    LinearAnimation progress

--- animations/LinearAnimation.js
+++ animations/LinearAnimation.js
@@ -16,23 +16,36 @@ class LinearAnimation extends Animation {
 		super(scene, time);
     this.controlPoints = controlPoints;
 		this.currentPoint = 0;
+		this.vecDist = [];
+		this.vecPercentage = [];
+		this.time = time;
 
-		let totalDistance = 0;
-
-		for(let i = 1; i < this.controlPoints.length;  i++) {
-			let point1 = vec3.fromValues(this.controlPoints[i-1].x, this.controlPoints[i-1].y, this.controlPoints[i-1].z);
-			let point2 = vec3.fromValues(this.controlPoints[i].x, this.controlPoints[i].y, this.controlPoints[i].z);
-			totalDistance += vec3.distance(point1, point2);
-		}
-
-		let firstPoint = vec3.fromValues(controlPoints[0].x, controlPoints[0].y, controlPoints[0].z);
-
-		mat4.translate(this.transformation, this.transformation, firstPoint);
 
 		this.initBuffers();
   };
 
 	apply() {
+
+		let totalDistance = 0;
+		let timeFragment = 0;
+
+		for(let i = 1; i < this.controlPoints.length;  i++) {
+			let point1 = vec3.fromValues(this.controlPoints[i-1].x, this.controlPoints[i-1].y, this.controlPoints[i-1].z);
+			let point2 = vec3.fromValues(this.controlPoints[i].x, this.controlPoints[i].y, this.controlPoints[i].z);
+			let currentDist = vec3.create();
+			vec3.subtract(currentDist, point2, point1);
+			vecDist.push(currentDist);
+
+			totalDistance += vec3.length(currentDist);
+		}
+
+		for(let j = 0; j < vecDist.length; j++) {
+			this.vecPercentage.push(vec3.length(vecDist)/totalDistance);
+		}
+
+		let firstPoint = vec3.fromValues(this.controlPoints[0].x, this.controlPoints[0].y, this.controlPoints[0].z);
+
+		mat4.translate(this.transformation, this.transformation, firstPoint);
 
 		if(this.currTime <= this.time) {
 			this.previousPoint = this.currentPoint;
