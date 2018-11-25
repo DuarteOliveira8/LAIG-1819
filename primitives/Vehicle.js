eb0840fe13e3d2eb1cd8eb64e8dcfbe434ab9057
Pedrocssg <pedrokraktuga@gmail.com>
2018-11-25 19:54:38 +0000

    Vehicle completed

--- primitives/Vehicle.js
+++ primitives/Vehicle.js
@@ -15,7 +15,21 @@
          this.frameCylinderArm = new CoveredCylinder2(this.scene, 0.4, 0.15, 2, 64, 64);
          this.armHelix = new CoveredCylinder2(this.scene, 0.1, 0.1, 0.2, 64, 64);
          this.verticalBaseSupport = new CoveredCylinder2(this.scene, 0.2, 0.2, 1, 64, 64);
-         this.horizontalBaseSupport = new Cylinder2(this.scene, 0.2, 0.2, 1, 8, 8);
+         this.helix = new CoveredCylinder2(this.scene, 0.01, 0.1, 0.5, 64, 64);
+
+         this.droneBodyAppearance = new CGFappearance(this.scene);
+         this.droneBodyAppearance.setAmbient(0.4,0.4,0.4,1);
+      	 this.droneBodyAppearance.setDiffuse(0.4,0.4,0.4,1);
+      	 this.droneBodyAppearance.setSpecular(0.5,0.5,0.5,1);
+      	 this.droneBodyAppearance.setShininess(150);
+         this.droneBodyAppearance.loadTexture("../scenes/images/drone.jpg");
+
+         this.helixBodyAppearance = new CGFappearance(this.scene);
+         this.helixBodyAppearance.setAmbient(0.6,0.6,0.6,1);
+      	 this.helixBodyAppearance.setDiffuse(0.7,0.7,0.7,1);
+      	 this.helixBodyAppearance.setSpecular(0.7,0.7,0.7,1);
+      	 this.helixBodyAppearance.setShininess(150);
+         this.helixBodyAppearance.loadTexture("../scenes/images/metal.png");
 
          this.initBuffers();
      };
@@ -27,6 +41,7 @@
         // Main Frame
         this.scene.pushMatrix();
           this.scene.scale(1.5, 0.7, 1.5);
+          this.droneBodyAppearance.apply();
           this.mainFrameSphere.display();
         this.scene.popMatrix();
 
@@ -124,12 +139,170 @@
 
         // Vertical Support Frame
         this.scene.pushMatrix();
-          this.scene.scale(0.5,5,0.5);
-
-          this.scene.rotate(90*degToRad, 1, 0, 0);
-          this.scene.rotate(60*degToRad, 0, 1, 0);
+          this.scene.translate(-1,0,0.75);
+          this.scene.rotate(60*degToRad, 1, 0, 0);
+          this.scene.scale(0.5, 0.5, 1.5);
           this.verticalBaseSupport.display();
         this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(-1,0,-0.75);
+          this.scene.rotate(120*degToRad, 1, 0, 0);
+          this.scene.scale(0.5, 0.5, 1.5);
+          this.verticalBaseSupport.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(1,0,-0.75);
+          this.scene.rotate(120*degToRad, 1, 0, 0);
+          this.scene.scale(0.5, 0.5, 1.5);
+          this.verticalBaseSupport.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(1,0,0.75);
+          this.scene.rotate(60*degToRad, 1, 0, 0);
+          this.scene.scale(0.5, 0.5, 1.5);
+          this.verticalBaseSupport.display();
+        this.scene.popMatrix();
+
+        // Horizontal Support Frame
+        this.scene.pushMatrix();
+          this.scene.translate(1,-1.25,-1.5);
+          this.scene.scale(0.5, 0.5, 3);
+          this.verticalBaseSupport.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(-1,-1.25,-1.5);
+          this.scene.scale(0.5, 0.5, 3);
+          this.verticalBaseSupport.display();
+        this.scene.popMatrix();
+
+        // First Pair of Helix
+        this.scene.pushMatrix();
+          this.scene.translate(0,0.5,1.5);
+          this.scene.scale(1,0.5,3);
+          this.helixBodyAppearance.apply();
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(0,0.5,4.5);
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,1.5);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,4.5);
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,1.5);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,4.5);
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,1.5);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.translate(0,0.5,4.5);
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        // Second Pair of Helix
+        this.scene.pushMatrix();
+          this.scene.translate(-1.5,0.5,3);
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.translate(1.5,0.5,3);
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.translate(-1.5,0.5,3);
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.translate(1.5,0.5,3);
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.translate(-1.5,0.5,3);
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(180*degToRad, 0, 1, 0);
+          this.scene.translate(1.5,0.5,3);
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.translate(-1.5,0.5,3);
+          this.scene.rotate(90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.translate(1.5,0.5,3);
+          this.scene.rotate(-90*degToRad, 0, 1, 0);
+          this.scene.scale(1,0.5,3);
+          this.helix.display();
+        this.scene.popMatrix();
+
+
      }
 
      updateTexCoords(s, t) {};
